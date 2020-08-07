62b6c6e0ef3e0e25037b55f81a322166389f51b4
currencytype <68088971+currencytype@users.noreply.github.com>
2020-08-07 17:01:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 */
 
 const config = {
-	token: 'NjA5NzU5MzY4MzAwMzMxMDEw.XU7Yqg.b293rxJehYRH8wmj_JGC8UjU51g',
-	guild: '660284360733491211', // Your server ID where you'll use it.
-	log: '741409302899589181', // Log channel ID.
-	modmail: '741409748523548733', // Modmail category ID. You can leave it blank if you don't want to move it to a specific catgeory.
-	support: '738487927494934578', // Support Team role ID.
+	token: '',
+	guild: '', // Your server ID where you'll use it.
+	log: '', // Log channel ID.
+	modmail: '', // Modmail category ID. You can leave it blank if you don't want to move it to a specific catgeory.
+	support: '', // Support Team role ID.
 	maxThreads: 0, // The max amount of modmail threads in the guild. 0 would mean infinity.
 	autoreply: false, // Auto replies to the user without invoking the reply-command. It's set to false (default) right now, you can set it to true if you want it.
 };
